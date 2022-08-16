62505d0e134ecfc7bf229327ca1422c825ee49bc
negiFSD <sunilnegi217@rediffmail.com>
2022-08-16 18:18:30 +0530

    registration and login implemented with redux thunk

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Logo, FormRow, ToggleMember } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
-
+import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from "react-redux";
+import { loginUser, registerUser } from "../features/user/userSlice";
+import { useNavigate } from "react-router-dom";
 const initialState = {
   name: "",
   email: "",
@@ -11,24 +14,43 @@ const initialState = {
 };
 
 function Register() {
+  const navigate = useNavigate();
   const [values, setValues] = useState(initialState);
+  const dispatch = useDispatch()
+  const {isLoading, user} = useSelector((store)=>store.user)
+
+  useEffect(()=>{
+    if(user){
+      setTimeout(() => {
+        navigate('/');
+      }, 3000);
+      // toast.success(`welcome `)
+    }
+    
+  },[user, navigate])
+
+
 
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    console.log(name, value);
+    // console.log(name, value);
     setValues({ ...values, [name]: value });
   };
-
   const onSubmit = (e) => {
     e.preventDefault();
     const { name, email, password, isMember } = values;
     if (!email || !password || (!isMember && !name)) {
-      console.log("fill all required values");
+     toast.error("fill all required values");
       return;
     }
-
-    console.log(values);
+    if(isMember){
+      dispatch(loginUser({email: email, password:  password}))
+      return
+    }
+    else{
+      dispatch(registerUser({email:email, password:password, name:name}))
+    }
   };
 
   const toggleMember = () => {
@@ -72,8 +94,8 @@ function Register() {
           toggleMember={toggleMember}
         />
 
-        <button type="submit" className="btn btn-block">
-          submit
+        <button type="submit" className="btn btn-block" disabled={isLoading}>
+         {isLoading?`loading...`:`submit`} 
         </button>
       </form>
     </Wrapper>
